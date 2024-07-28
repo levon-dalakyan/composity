17427440b150fb6a2d6ae1186c65073598bddbac
Levon Dalakyan <wman8800@gmail.com>
2024-07-28 09:38:10 +0300

    add Reader

--- src/fantasy/Lazy.js
+++ src/fantasy/Lazy.js
@@ -1,57 +1,57 @@
 class Lazy {
-  constructor(computation) {
-    this.computation = computation;
-  }
+    constructor(computation) {
+        this.computation = computation;
+    }
 
-  evaluate() {
-    return this.computation();
-  }
+    evaluate() {
+        return this.computation();
+    }
 
-  extract() {
-    return this["fantasy-land/extract"]();
-  }
+    extract() {
+        return this["fantasy-land/extract"]();
+    }
 
-  map(fn) {
-    return this["fantasy-land/map"](fn);
-  }
+    map(fn) {
+        return this["fantasy-land/map"](fn);
+    }
 
-  ap(other) {
-    return this["fantasy-land/ap"](other);
-  }
+    ap(other) {
+        return this["fantasy-land/ap"](other);
+    }
 
-  chain(fn) {
-    return this["fantasy-land/chain"](fn);
-  }
+    chain(fn) {
+        return this["fantasy-land/chain"](fn);
+    }
 
-  extend(fn) {
-    return this["fantasy-land/extend"](fn);
-  }
+    extend(fn) {
+        return this["fantasy-land/extend"](fn);
+    }
 
-  static of(value) {
-    return Lazy["fantasy-land/of"](value);
-  }
+    static of(value) {
+        return Lazy["fantasy-land/of"](value);
+    }
 
-  ["fantasy-land/extract"]() {
-    return this.computation();
-  }
+    ["fantasy-land/extract"]() {
+        return this.computation();
+    }
 
-  ["fantasy-land/map"](fn) {
-    return new Lazy(() => fn(this.computation()));
-  }
+    ["fantasy-land/map"](fn) {
+        return new Lazy(() => fn(this.computation()));
+    }
 
-  ["fantasy-land/ap"](other) {
-    return new Lazy(() => other.evaluate()(this.computation()));
-  }
+    ["fantasy-land/ap"](other) {
+        return new Lazy(() => other.evaluate()(this.computation()));
+    }
 
-  ["fantasy-land/chain"](fn) {
-    return new Lazy(() => fn(this.computation()).evaluate());
-  }
+    ["fantasy-land/chain"](fn) {
+        return new Lazy(() => fn(this.computation()).evaluate());
+    }
 
-  ["fantasy-land/extend"](fn) {
-    return new Lazy(() => fn(this));
-  }
+    ["fantasy-land/extend"](fn) {
+        return new Lazy(() => fn(this));
+    }
 
-  static ["fantasy-land/of"](value) {
-    return new Lazy(() => value);
-  }
+    static ["fantasy-land/of"](value) {
+        return new Lazy(() => value);
+    }
 }
