2308b01859e25de459ad8f23151185dd21475f60
Levon Dalakyan <wman8800@gmail.com>
2024-08-06 18:39:56 +0300

    add exports for fp/containers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
-export * from "./utils";
-
-export { keys } from "./keys";
-export { values } from "./values";
-export { mergeAll } from "./merge-all";
-export { clone } from "./clone";
+export * from "./fp";
+export * from "./iterators";
