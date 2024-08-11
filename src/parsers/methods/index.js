6f5372f768f22a39ff9b358ce4155ff8d4841a0f
Levon Dalakyan <wman8800@gmail.com>
2024-08-11 21:10:16 +0300

    add exports to iterators and parsers

---
+++ src/parsers/methods/index.js
@@ -0,0 +1,9 @@
+export { pEnd } from "./pEnd";
+export { pMany } from "./pMany";
+export { pMany1 } from "./pMany1";
+export { pOpt } from "./pOpt";
+export { pOr } from "./pOr";
+export { pRepeat } from "./pRepeat";
+export { pSeq } from "./pSeq";
+export { pTag } from "./pTag";
+export { pTake } from "./pTake";
--- src/parsers/tags/index.js
+++ src/parsers/tags/index.js
@@ -1,5 +1,7 @@
 import { pTag } from "../methods/pTag/pTag.js";
 
+export const any = pTag(/./);
+
 export const digit = pTag(/[0-9]/);
 export const digit1 = pTag(/[1-9]/);
 export const digits = pTag(/[0-9]+/);
@@ -20,4 +22,29 @@ export const whitespace = pTag(/\s/);
 export const whitespaces = pTag(/\s+/);
 export const optWhitespaces = pTag(/\s*/);
 
-export const any = pTag(/./);
+export const dot = pTag(/\./);
+export const comma = pTag(/,/);
+
+export const colon = pTag(/:/);
+export const semicolon = pTag(/;/);
+
+export const dash = pTag(/-/);
+export const underscore = pTag(/_/);
+
+export const slash = pTag(/\//);
+export const backSlash = pTag(/\\/);
+
+export const leftParen = pTag(/\(/);
+export const rightParen = pTag(/\)/);
+export const leftBrace = pTag(/\{/);
+export const rightBrace = pTag(/\}/);
+export const leftBracket = pTag(/\[/);
+export const rightBracket = pTag(/\]/);
+
+export const equals = pTag(/=/);
+export const not = pTag(/!/);
+export const question = pTag(/\?/);
+export const at = pTag(/@/);
+
+export const singleQuote = pTag(/'/);
+export const doubleQuote = pTag(/"/);
