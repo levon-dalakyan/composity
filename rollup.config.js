eb76b01775ce386d69db086dee4e3d73f185762f
Levon Dalakyan <wman8800@gmail.com>
2024-08-24 07:46:07 +0300

    improve lib importing system

--- rollup.config.js
+++ rollup.config.js
@@ -2,24 +2,50 @@ import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 
-export default {
-    input: "src/index.js",
-    output: {
-        dir: "dist",
-        format: "es",
-        preserveModules: true,
-        preserveModulesRoot: "src",
+export default [
+    {
+        input: "src/index.js",
+        output: {
+            dir: "dist",
+            format: "es",
+            preserveModules: true,
+            preserveModulesRoot: "src",
+        },
+        plugins: [
+            resolve({
+                extensions: [".js"],
+                preferBuiltins: true,
+            }),
+            babel({
+                babelHelpers: "bundled",
+                exclude: "node_modules/**",
+            }),
+            commonjs(),
+        ],
+        external: [],
     },
-    plugins: [
-        resolve({
-            extensions: [".js"],
-            preferBuiltins: true,
-        }),
-        babel({
-            babelHelpers: "bundled",
-            exclude: "node_modules/**",
-        }),
-        commonjs(),
-    ],
-    external: [],
-};
+    {
+        input: {
+            fp: "src/fp/index.js",
+            iterators: "src/iterators/index.js",
+            lens: "src/lens/index.js",
+        },
+        output: {
+            dir: "dist",
+            format: "es",
+            preserveModules: true,
+            preserveModulesRoot: "src",
+        },
+        plugins: [
+            resolve({
+                extensions: [".js"],
+                preferBuiltins: true,
+            }),
+            babel({
+                babelHelpers: "bundled",
+                exclude: "node_modules/**",
+            }),
+            commonjs(),
+        ],
+    },
+];
